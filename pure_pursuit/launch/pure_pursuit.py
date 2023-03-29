af90cac2b8e935a18d2ae24f03abfac864d16769
griffinaddison <gaddison@seas.upenn.edu>
2023-03-29 13:16:38 -0400

    ambitious v1 params

--- pure_pursuit/launch/pure_pursuit.py
+++ pure_pursuit/launch/pure_pursuit.py
@@ -19,15 +19,15 @@ def generate_launch_description():
         executable='pure_pursuit_node',
         name='pure_pursuit_node',
         parameters=[
-            {"lookahead_distance": 2.25}, #levine 1.25
-            {"velocity": 5.5}, #levine 9.0
-            {"speed_lookahead_distance": 3.25}, #levine 2.0
-            {"brake_gain": 3.4}, #levine 12.0
+            {"lookahead_distance": 2.60}, #levine 1.25
+            {"velocity": 5.7}, #levine 9.0
+            {"speed_lookahead_distance": 3.8}, #levine 2.0
+            {"brake_gain": 3.80}, #levine 12.0
             {"visualize": True},
             {"wheel_base": 0.32},
-            {"acceleration_lookahead_distance": 5.0},
+            {"acceleration_lookahead_distance": 6.5},
             {"curvature_thresh": 0.1},
-            {"accel_gain": 0.0}
+            {"accel_gain": 9.0}
         ]
         )
 
