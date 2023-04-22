df8010b69630a5fa43f5130b3676408111f34b1c
griffinaddison <gaddison@seas.upenn.edu>
2023-04-21 22:04:32 -0400

    final commits

--- pure_pursuit/launch/pure_pursuit.py
+++ pure_pursuit/launch/pure_pursuit.py
@@ -19,15 +19,15 @@ def generate_launch_description():
         executable='pure_pursuit_node',
         name='pure_pursuit_node',
         parameters=[
-            {"lookahead_distance": 2.60}, #levine 1.25
+            {"lookahead_distance": 2.70}, #levine 1.25
             {"velocity": 5.7}, #levine 9.0
-            {"speed_lookahead_distance": 3.8}, #levine 2.0
-            {"brake_gain": 3.80}, #levine 12.0
+            {"speed_lookahead_distance": 3.7}, #levine 2.0
+            {"brake_gain": 4.10}, #levine 12.0
             {"visualize": False},
             {"wheel_base": 0.32},
             {"acceleration_lookahead_distance": 6.5},
             {"curvature_thresh": 0.1},
-            {"accel_gain": 9.0}
+            {"accel_gain": 11.0}
         ]
         )
 
